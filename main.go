8101d1047d03ad4d8a1d0e4438fd320249f7120c
kaveh <kaveh.fazaeli@gmail.com>
2020-04-08 23:00:32 +0430

    socket.io start and get first message

--- main.go
+++ main.go
@@ -1,12 +1,52 @@
 package main
 
 import (
-	"d2g-server/presenter"
+	"bodybuilding/src/lib/common"
 	"fmt"
+	"github.com/googollee/go-socket.io"
+	"log"
+	"net/http"
 )
 
 func main() {
-	rMain := presenter.SetupRouter()
-	err := rMain.Run(":8080")
-	fmt.Println(err)
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		return nil
+	})
+	server.OnEvent("/", "sign-in", func(s socketio.Conn, msg common.JSON) {
+		fmt.Println("notice:", msg)
+		//s.Emit("reply", "have "+msg)
+	})
+	server.OnEvent("/", "loggedInPlayer", func(s socketio.Conn, msg string) {
+		fmt.Println("notice:", msg)
+		s.Emit("reply", "have "+msg)
+	})
+	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
+		s.SetContext(msg)
+		return "recv " + msg
+	})
+	server.OnEvent("/", "bye", func(s socketio.Conn) string {
+		last := s.Context().(string)
+		s.Emit("bye", last)
+		s.Close()
+		return last
+	})
+	server.OnError("/", func(s socketio.Conn, e error) {
+		fmt.Println("meet error:", e)
+	})
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		fmt.Println("closed", reason)
+	})
+	go server.Serve()
+	defer server.Close()
+
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Println("Serving at localhost:8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
