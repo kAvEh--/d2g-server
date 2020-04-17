9a15fd153d4f0576ab6fbf52835a5e4a43be855b
kaveh <kaveh.fazaeli@gmail.com>
2020-04-17 23:21:36 +0430

    Match Data Model

--- model/match.go
+++ model/match.go
@@ -1,8 +1,45 @@
 package model
 
+import uuid "github.com/satori/go.uuid"
+
 type Room struct {
 	IDv2
-	RoomNum int  `json:"room_num"`
-	Player1 User `json:"player_1"`
-	Player2 User `json:"player_2"`
+	RoomNum          int       `json:"room_num"`
+	P1ID             uuid.UUID `json:"p1ID"`
+	P2ID             uuid.UUID `json:"p2ID"`
+	P1Name           string    `json:"p1_name"`
+	P2Name           string    `json:"p2_name"`
+	P1Level          int       `json:"p1_level"`
+	P2Level          int       `json:"p2_level"`
+	P1Xp             int       `json:"p1_xp"`
+	P2Xp             int       `json:"p2_xp"`
+	P1Shirt          int       `json:"p1_shirt"`
+	P2Shirt          int       `json:"p2_shirt"`
+	P1Avatar         int       `json:"p1_avatar"`
+	P2Avatar         int       `json:"p2_avatar"`
+	P1WinRate        float32   `json:"p1_win_rate"`
+	P1Formation      int       `json:"p1_formation"`
+	Level            int       `json:"level"`
+	LastTouch        int       `json:"last_touch"`
+	P1Goals          int       `json:"p1_goals"`
+	P2Goals          int       `json:"p2_goals"`
+	RoundNum         int       `json:"round_num"`
+	GameState        int       `json:"game_state"`
+	IsP1First        bool      `json:"is_p1_first"`
+	GoalToWin        int       `json:"goal_to_win"`
+	IsP1Ready        bool      `json:"is_p1_ready"`
+	IsP2Ready        bool      `json:"is_p2_ready"`
+	P1PlayerShooting int       `json:"p1_player_shooting"`
+	P2PlayerShooting int       `json:"p2_player_shooting"`
+	P1X              float32   `json:"p1_x"`
+	P1Y              float32   `json:"p1_y"`
+	P2X              float32   `json:"p2_x"`
+	P2Y              float32   `json:"p2_y"`
+	P1DataSync       bool      `json:"p1_data_sync"`
+	P2DataSync       bool      `json:"p2_data_sync"`
+	IsMatchReady     bool      `json:"is_match_ready"`
+	IsWinner         bool      `json:"is_winner"`
+	HalfStatus       int       `json:"half_status"`
+	GaolerPosition   int       `json:"gaoler_position"`
+	GameType         int       `json:"game_type"`
 }
--- presenter/match.go
+++ presenter/match.go
@@ -17,76 +17,12 @@ func findMatch(s socketio.Conn, msg common.JSON) {
 	gameType, _ := strconv.Atoi(msg["type"].(string))
 	fmt.Println(userId, ":::", lvl, ":::", gameType)
 
-	var user model.User
-	var player model.Player
-	//user.ID = uuid.UUID{}
-	user.DailyCoin = true
-	player.ID = 1
-	player.WinCount = 10
-	player.GameCount = 20
-	player.WinRate = 50
-	player.CleanSheet = 15
-	player.Formation = 2
-	player.Lineup = "B"
-	player.Shirt = 1
-	player.Name = "kAvEh"
-	player.AvatarID = 5
-	player.UserName = "kAvEh"
-	player.Goals = 100
-	player.WinInRaw = 5
-	player.Coin = 2000
-	var level model.Level
-	level.Level = 6
-	level.XP = 500
-	player.Level = &level
-	var ach model.Achievement
-	ach.WinInRaw = 2
-	ach.CleanSheet = 1
-	player.Achievement = &ach
-	var m1 model.Member
-	var m2 model.Member
-	var m3 model.Member
-	var m4 model.Member
-	var m5 model.Member
-	m1.ShirtNum = 1
-	m1.Size = 1
-	m1.Speed = 0
-	m1.Stamina = 2
-	m2.ShirtNum = 2
-	m2.Size = 2
-	m2.Speed = 0
-	m2.Stamina = 2
-	m3.ShirtNum = 3
-	m3.Size = 1
-	m3.Speed = 1
-	m3.Stamina = 2
-	m4.ShirtNum = 4
-	m4.Size = 1
-	m4.Speed = 0
-	m4.Stamina = 1
-	m5.ShirtNum = 5
-	m5.Size = 0
-	m5.Speed = 0
-	m5.Stamina = 2
-	var tmp []model.Member
-	tmp = append(tmp, m1, m2, m3, m4, m5)
-	player.Players = &tmp
-	shirts := make([]model.Shirt, 24)
-	for i, shirt := range shirts {
-		shirt.ShirtID = i
-		shirt.HasShirt = i%4 == 0
-		shirts[i] = shirt
-	}
-	player.Shirts = &shirts
-
-	user.Player = &player
-	//user.Player.Level.Level = 6
-	//user.Player.Level.XP = 500
+	var room model.Room
 
 	D2GServer.JoinRoom("", userId, s)
 	fmt.Println(D2GServer.Rooms(""))
 
-	b, err := json.Marshal(user)
+	b, err := json.Marshal(room)
 	if err != nil {
 		fmt.Println(err)
 		return
